3af542e4dcd803c98d01c98229aec0d89a858aa3
Hidenori TAKESHITA <taketin@linecorp.com>
2022-06-16 02:21:45 +0900

    Initial commit

---
+++ docc_sample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  docc_sample
+//
+//  Created by ST14573 on 2022/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ docc_sample/docc_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  docc_sampleApp.swift
+//  docc_sample
+//
+//  Created by ST14573 on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct docc_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
